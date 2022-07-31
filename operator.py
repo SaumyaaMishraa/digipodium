2e5f26527b11b09f122f489b7bd70d9eda280b3c
saumya mishra <saumyamishrasm24@gmail.com>
2022-07-31 14:04:55 +0530

    basics

---
+++ operator.py
@@ -0,0 +1,15 @@
+a= 20
+b= 10
+
+print(a+b)
+print(a-b)
+print(a/b) #float division
+print(a//b) #interger division(quotient) or floor division
+print(a%b) #remainder
+print(a*b)
+print(a**b) #exponent
+
+# membership operator 
+#x in y
+#x in not y
+#
--- variables.py
+++ variables.py
@@ -1,5 +1,33 @@
+# comments
+
 name='saumya mishra'
 college='bbdec'
-age= 22
+age= 22 
 pi=3.14
-print (name,college,age,pi)
+print (name,college,age,pi , type(age)) #(type) can take one at a time 
+x= 25
+y= True
+x2= 15.11
+y2= -10
+x3= 87.7e100
+y3= False
+m= None
+a=[1,2,3,4,5,6,7,8,9]    # mutable (data can be changed) 
+b={1,2,1.2,"python"}    # (operation like union and intersection can be done)
+c=("red", "yellow", "green")    #immutable (data can be changed)
+car_info= {"brand":"ford","model":"mustang", "year":"1992" }
+print(car_info)
+print("pink" in c)
+print("red" in c) # checking if the word is in the tuple or not
+print (x, type(x))
+print (y, type(y))
+print (x2, type(x2))
+print (y2, type(y2))
+print (x3, type(x3))
+print (y3, type(y3))
+print (m, type(m))
+print (a, type(a))
+print (b, type(b))
+print (c, type(c))
+sum = x+x2
+print (sum)
